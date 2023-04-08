7d776cc117ca9194b1c837632e0d61d0069815fc
Ni1ba <danila.baldin@mail.ru>
2023-04-08 22:19:29 +0300

    new random

--- src/pSaper/clsSaperMain.java
+++ src/pSaper/clsSaperMain.java
@@ -1,7 +1,11 @@
 package pSaper;
+import java.util.Random;
+
 
 public class clsSaperMain {
 	public static void main(String[] args){
+		final Random a = new Random();
 		
+		System.out.print(a.nextLong(9));
 	}
 }
